9a7f8789b34781724ab62ce5fc5826f50189c83a
gfCrova <giancrova.cl5@gmail.com>
2023-05-31 21:17:56 -0300

    refactor DTOusers to autenticacionUsuarios

---
+++ src/main/java/com/alura/foro/domain/usuario/DTOAutenticacionUsuarios.java
@@ -0,0 +1,4 @@
+package com.alura.foro.domain.usuario;
+
+public record DTOAutenticacionUsuarios(String nombre, String email, String contrasena) {
+}
--- src/main/java/com/alura/foro/domain/usuario/DTOUsuarios.java
+++
@@ -1,4 +0,0 @@
-package com.alura.foro.domain.usuario;
-
-public record DTOUsuarios(String nombre, String email, String contrasena) {
-}
