9e8a0a24ef900a59903da0bccbde7908a27c0761
gfCrova <giancrova.cl5@gmail.com>
2023-05-31 21:15:34 -0300

    add Entity annotations implements UserDetails and all methods

--- src/main/java/com/alura/foro/domain/usuario/Usuario.java
+++ src/main/java/com/alura/foro/domain/usuario/Usuario.java
@@ -1,7 +1,22 @@
 package com.alura.foro.domain.usuario;
 
-public class Usuario {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
 
+@Table(name = "usuarios")
+@Entity(name = "usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario implements UserDetails {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nombre;
 	private String email;
@@ -32,36 +47,38 @@ public class Usuario {
 		return true;
 	}
 
-	public Long getId() {
-		return id;
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getContrasena() {
+	@Override
+	public String getPassword() {
 		return contrasena;
 	}
 
-	public void setContrasena(String contrasena) {
-		this.contrasena = contrasena;
+	@Override
+	public String getUsername() {
+		return nombre;
 	}
 
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }
