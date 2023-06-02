1000da13bfe848cafe3a91307f0dbb196a959d44
gfCrova <giancrova.cl5@gmail.com>
2023-06-01 23:57:17 -0300

    modificando datos de salida al registrar topicos

--- src/main/java/com/alura/foro/domain/topico/DTORegistrarTopico.java
+++ src/main/java/com/alura/foro/domain/topico/DTORegistrarTopico.java
@@ -1,2 +1,13 @@
-package com.alura.foro.domain.topico;public class DTORegistrarTopico {
+package com.alura.foro.domain.topico;
+
+import com.alura.foro.domain.curso.Curso;
+import com.alura.foro.domain.curso.DatosCurso;
+import com.alura.foro.domain.usuario.DatosUsuario;
+import jakarta.validation.constraints.NotBlank;
+
+public record DTORegistrarTopico(
+        @NotBlank String titulo,
+        @NotBlank String mensaje,
+        Long usuario,
+        Long curso) {
 }
