41869ab37e1ea06018e04733a1c9614acf345a45
gfCrova <giancrova.cl5@gmail.com>
2023-06-01 23:55:33 -0300

    add dto listar topicos

---
+++ src/main/java/com/alura/foro/domain/topico/DTOListarTopicos.java
@@ -0,0 +1,13 @@
+package com.alura.foro.domain.topico;
+
+import com.alura.foro.domain.curso.Curso;
+import com.alura.foro.domain.usuario.Usuario;
+
+import java.time.LocalDateTime;
+
+public record DTOListarTopicos(Long id, String titulo, String mensaje, LocalDateTime fechaCreacion, String status, String usuario, String curso) {
+
+    public DTOListarTopicos(Topico topico) {
+        this(topico.getId(), topico.getTitulo(), topico.getMensaje(), topico.getFechaCreacion(), topico.getStatus().toString(), topico.getUsuario().getNombre(), topico.getCurso().getNombre());
+    }
+}
