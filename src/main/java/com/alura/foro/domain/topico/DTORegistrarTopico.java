c855103742749f4010794a2fdf7cf2cdd6f9aeb4
gfCrova <giancrova.cl5@gmail.com>
2023-06-02 22:43:37 -0300

    type model Usuario & Curso

--- src/main/java/com/alura/foro/domain/topico/DTORegistrarTopico.java
+++ src/main/java/com/alura/foro/domain/topico/DTORegistrarTopico.java
@@ -3,11 +3,12 @@ package com.alura.foro.domain.topico;
 import com.alura.foro.domain.curso.Curso;
 import com.alura.foro.domain.curso.DatosCurso;
 import com.alura.foro.domain.usuario.DatosUsuario;
+import com.alura.foro.domain.usuario.Usuario;
 import jakarta.validation.constraints.NotBlank;
 
 public record DTORegistrarTopico(
         @NotBlank String titulo,
         @NotBlank String mensaje,
-        Long usuario,
-        Long curso) {
+        Usuario usuario,
+        Curso curso) {
 }
