b5a844f3649ab466d9af902bb0428935b093dc72
gfCrova <giancrova.cl5@gmail.com>
2023-06-02 22:39:01 -0300

    update methods put and delete topicos

--- src/main/java/com/alura/foro/controller/TopicosController.java
+++ src/main/java/com/alura/foro/controller/TopicosController.java
@@ -1,15 +1,19 @@
 package com.alura.foro.controller;
 
+import com.alura.foro.domain.curso.Curso;
 import com.alura.foro.domain.curso.DTORegistrarCurso;
 import com.alura.foro.domain.curso.DatosCurso;
+import com.alura.foro.domain.topico.DTOActualizarTopico;
 import com.alura.foro.domain.topico.DTOListarTopicos;
 import com.alura.foro.domain.topico.DTORegistrarTopico;
 import com.alura.foro.domain.respuesta.DTORespuestaTopico;
 import com.alura.foro.domain.topico.Topico;
 import com.alura.foro.domain.usuario.DatosUsuario;
+import com.alura.foro.domain.usuario.Usuario;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 import com.alura.foro.repository.TopicoRepository;
@@ -36,8 +40,8 @@ public class TopicosController {
                 topico.getMensaje(),
                 topico.getFechaCreacion(),
                 topico.getStatus(),
-                topico.getUsuario(),
-                topico.getCurso()
+                topico.getUsuario().getNombre(),
+                topico.getCurso().getNombre()
         );
         URI url = uri.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
         return ResponseEntity.created(url).body(datosDeRespuestaTopico);
@@ -46,5 +50,46 @@ public class TopicosController {
     @GetMapping
     public List<DTOListarTopicos> listadoTopicos() {
         return topicoRepository.findAll().stream().map(DTOListarTopicos::new).toList();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DTORespuestaTopico> listarTopicosPorId(@PathVariable Long id) {
+        Topico topico = topicoRepository.getReferenceById(id);
+        var datosTopico = new DTORespuestaTopico(
+                topico.getId(),
+                topico.getTitulo(),
+                topico.getMensaje(),
+                topico.getFechaCreacion(),
+                topico.getStatus(),
+                topico.getUsuario().getNombre(),
+                topico.getCurso().getNombre()
+        );
+        return ResponseEntity.ok(datosTopico);
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity<DTORespuestaTopico> actualizarTopico(@RequestBody @Valid DTOActualizarTopico datosActualizar) {
+        Topico topico = topicoRepository.getReferenceById(datosActualizar.id());
+        topico.actualizarDatos(datosActualizar);
+        return ResponseEntity.ok( new DTORespuestaTopico(
+                topico.getId(),
+                topico.getTitulo(),
+                topico.getMensaje(),
+                topico.getFechaCreacion(),
+                topico.getStatus(),
+                topico.getUsuario().getNombre(),
+                topico.getCurso().getNombre()
+        ));
+    }
+
+    // **** DELETE ****
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity eliminarTopico(@PathVariable Long id) {
+        Topico topico = topicoRepository.getReferenceById(id);
+        //topico.desactivarTopico();  // Delete Lógico (No en DB)
+        topicoRepository.delete(topico);
+        return ResponseEntity.noContent().build();
     }
 }
