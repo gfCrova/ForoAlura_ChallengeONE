172ba2d05aef2de6c3878a7a0bff6d317931d50a
gfCrova <giancrova.cl5@gmail.com>
2023-06-04 00:39:18 -0300

    completando los métodos crud de CursoController

--- src/main/java/com/alura/foro/controller/CursoController.java
+++ src/main/java/com/alura/foro/controller/CursoController.java
@@ -1,12 +1,14 @@
 package com.alura.foro.controller;
 
-import com.alura.foro.domain.curso.Curso;
-import com.alura.foro.domain.curso.DTOListarCursos;
-import com.alura.foro.domain.curso.DTORegistrarCurso;
+import com.alura.foro.domain.curso.*;
+import com.alura.foro.domain.respuesta.DTORespuestaTopico;
+import com.alura.foro.domain.topico.DTOActualizarTopico;
+import com.alura.foro.domain.topico.Topico;
 import com.alura.foro.repository.CursoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -35,5 +37,25 @@ public class CursoController {
     @GetMapping
     public List<DTOListarCursos> listadoCursos() {
         return cursoRepository.findAll().stream().map(DTOListarCursos::new).toList();
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity actualizarTopico(@RequestBody @Valid DTOActualizarCurso datosActualizar) {
+        Curso curso = cursoRepository.getReferenceById(datosActualizar.id());
+        curso.actualizarDatos(datosActualizar);
+            return ResponseEntity.ok( new DTOListarCursos(
+                    curso.getId(),
+                    curso.getNombre(),
+                    curso.getCategoria()
+        ));
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity eliminarTopico(@PathVariable Long id) {
+        Curso curso = cursoRepository.getReferenceById(id);
+        cursoRepository.delete(curso);
+        return ResponseEntity.noContent().build();
     }
 }
