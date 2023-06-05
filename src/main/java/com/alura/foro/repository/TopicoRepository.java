a9d17fc4fd33d63602b1146014ee30e69886d374
gfCrova <giancrova.cl5@gmail.com>
2023-06-05 00:58:36 -0300

    add method findByActivoTrue(Page)

--- src/main/java/com/alura/foro/repository/TopicoRepository.java
+++ src/main/java/com/alura/foro/repository/TopicoRepository.java
@@ -1,8 +1,11 @@
 package com.alura.foro.repository;
 
 import com.alura.foro.domain.topico.Topico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
+    Page<Topico> findByActivoTrue(Pageable paginacion);
 }
