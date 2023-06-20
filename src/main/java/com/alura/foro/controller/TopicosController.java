29081dd495a51338c649c490958e3c40f8efc353
gfCrova <giancrova.cl5@gmail.com>
2023-06-20 14:17:59 -0300

    delete imp sin uso

--- src/main/java/com/alura/foro/controller/TopicosController.java
+++ src/main/java/com/alura/foro/controller/TopicosController.java
@@ -11,7 +11,6 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
